c8f58e534aa13a95416c5f08255128e24b10f73d
aquirozc <alejandroqc03@outlook.com>
2024-01-27 18:11:08 -0600

    Added input validity checks

--- lib/src/main/java/com/aquirozc/jxinputemulator/VirtualXUSB.java
+++ lib/src/main/java/com/aquirozc/jxinputemulator/VirtualXUSB.java
@@ -20,36 +20,56 @@ public class VirtualXUSB extends VirtualController {
 		report.sThumbRY = 0;
 	}
 	
-	public void pressButton(int button) {
-		report.wButtons = report.wButtons | button ;
+	public void pressButton(XUSBButton button) {
+		report.wButtons = report.wButtons | button.flag ;
 	}
 	
-	public void releaseButton(int button) {
-		report.wButtons = report.wButtons & ~button;
+	public void releaseButton(XUSBButton button) {
+		report.wButtons = report.wButtons & ~button.flag;
 	}
 	
 	public void setLeftTrigger(int value) {
+		
+		if(value < 0 || value > 255) {
+			throw new IllegalArgumentException("Input must be an integer between 0 and 255. But was " + value);
+		}
+		
 		report.bLeftTrigger = value;
 	}
 	
 	public void setRightTrigger(int value) {
+		
+		if(value < 0 || value > 255) {
+			throw new IllegalArgumentException("Input must be an integer between 0 and 255. But was " + value);
+		}
+		
 		report.bRightTrigger = value;
 	}
 	
 	public void setLeftTrigger(float value) {
+		
+		if(value < 0 || value > 1) {
+			throw new IllegalArgumentException("Input must be a float between 0 and 1. But was " + value);
+		}
+		
 		report.bLeftTrigger = Math.round(255* value);
 	}
 	
 	public void setRightTrigger(float value) {
+		
+		if(value < 0 || value > 1) {
+			throw new IllegalArgumentException("Input must be a float between 0 and 1. But was " + value);
+		}
+		
 		report.bRightTrigger = Math.round(255* value);
 	}
 	
-	public void setLeftJoystick(int x, int y) {
+	public void setLeftJoystick(short x, short y) {
 		report.sThumbLX = x;
 		report.sThumbLY = y;
 	}
 	
-	public void setLRightJoystick(int x, int y) {
+	public void setLRightJoystick(short x, short y) {
 		report.sThumbRX = x;
 		report.sThumbRY = y;
 	}
@@ -57,7 +77,6 @@ public class VirtualXUSB extends VirtualController {
 	public void update() {
 		client.vigem_target_x360_update(bus,device,report);
 	}
-	
 
 	@Override
 	public Pointer allocateTarget() {
